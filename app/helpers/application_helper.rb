2ee52332e9e9a2838137421b27ec85507a370a65
Apo <jiyuny@yahoo.fr>
2013-12-03 01:31:00 +0100

    Merge branch 'master' of https://github.com/apo-j/app_shop
    Conflicts:
    	app/helpers/app_engine.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 module ApplicationHelper
-  include APPEngine::DataSearch
 
-  def get_navbar_by_org_obj(org_id, obj_name)
-    search_app_data_by_obj_name(org_id, obj_name,page:1, size:0).to_json
-  end
 end
